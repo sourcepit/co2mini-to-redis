8b05f4a5baf6bac0ed197793146b9ca8ff1b1d2a
Bernd Vogt <bernd.vogt@sourcepit.org>
2020-01-12 18:50:40 +0100

    feat: publish temperature and co2 to redis

--- src/main.rs
+++ src/main.rs
@@ -13,19 +13,24 @@ use common_failures::prelude::*;
 
 use co2mini::Co2Mini;
 use co2mini::Value;
+use redis::Commands;
 
 fn run() -> Result<()> {
+    let mut redis = redis::Client::open("redis://127.0.0.1/")?.get_connection()?;
+
     let co2mini = Co2Mini::open()?;
     loop {
         match co2mini.read()? {
             Value::Temperature(v) => {
                 println!("Temperature {}", v);
+                redis.publish("co2mini/temperature", v)?;
             }
             Value::Co2(v) => {
                 println!("CO2 {}", v);
+                redis.publish("co2mini/co2", v)?;
             }
             Value::Unknown(_) => {}
-        };
+        }
     }
 }
 
